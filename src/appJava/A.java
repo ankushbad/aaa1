a4e890950457514ab778f40383b96293c5bf4e50
ankushbad <badoleankush@gmail.com>
2025-11-01 18:19:33 +0530

    aa

---
+++ src/appJava/A.java
@@ -0,0 +1,14 @@
+package appJava;
+
+public class A {
+    public int x=5;
+    int y =10;
+
+    int z= 33 ;
+
+    public static void main(String[] args) {
+            A a = new A ();
+        System.out.println(a.x);
+        System.out.println(a.y);
+    }
+}
